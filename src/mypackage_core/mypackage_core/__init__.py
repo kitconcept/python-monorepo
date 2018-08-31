88797b35ae270570d271734443f4a502b04f90eb
Timo Stollenwerk <tisto@plone.org>
2018-08-31 15:15:34 +0200

    Set version for all packages to 1.2.0, add hello function.

--- src/mypackage_core/mypackage_core/__init__.py
+++ src/mypackage_core/mypackage_core/__init__.py
@@ -1,5 +1,9 @@
 """mypackage_core - An opinionated, minimal cookiecutter template for Python packages"""
 
-__version__ = '1.0.0'
+__version__ = '1.2.0'
 __author__ = 'Timo Stollenwerk <stollenwerk@kitconcept.com>'
 __all__ = []
+
+
+def hello():
+    return __version__
